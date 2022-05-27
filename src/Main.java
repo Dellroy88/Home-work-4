d678b31be8ff5edf3f0f08a0c6482ef55c3621c7
Семья Ким <dell200288@yandex.ru>
2022-05-27 21:05:03 +0300

    дз4 от Владимира

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,7 @@
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Задача 1");
@@ -9,14 +13,20 @@ public class Main {
         for (int i = 10; i > 0; i--) {
             System.out.println("Цикл " + i);
         }
-        System.out.println("Задание 2");
+        System.out.println("Задача 2");
         for (int f = 5; f < 31; f = f + 7) {
             System.out.println("Сегодня пятница " + f + " число, необходимо подготовить отчёт");
         }
-        System.out.println("Задание 3");
-        for (int y = 1896; y < 2122; y = y + 79) {
-            System.out.println(y);
-            }
+        System.out.println("Задача 3");
+        int currentYear = LocalDate.now().getYear();
+
+        int start = currentYear - 200;
+        int end = currentYear + 100;
+
+        for (int year = start; year <= end; year++) {
+            if (year % 79 == 0)
+            System.out.println(year);
+        }
 
     }
 }
