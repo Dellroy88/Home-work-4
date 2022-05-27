24688e6737451bbabde8ede74876337bc758f9a6
Семья Ким <dell200288@yandex.ru>
2022-05-27 20:17:47 +0300

    дз4 от Владимира

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Задача 1");
+        int p = 0;
+        while (p < 10) {
+            p = p + 1;
+            System.out.println("Цикл " + p);
+        }
+        for (int i = 10; i > 0; i--) {
+            System.out.println("Цикл " + i);
+        }
+        System.out.println("Задание 2");
+        for (int f = 5; f < 31; f = f + 7) {
+            System.out.println("Сегодня пятница " + f + " число, необходимо подготовить отчёт");
+        }
+        System.out.println("Задание 3");
+        for (int y = 1896; y < 2122; y = y + 79) {
+            System.out.println(y);
+            }
+
     }
 }
